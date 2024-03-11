a0fcd359fff0b48210a216252dc3317de95e1a59
DahyeShin <dhye0513@gmail.com>
2024-03-11 22:29:37 +0900

    레이아웃 수정 및 lnb 스크립트 작업중

--- bo/resources/js/ui.common.js
+++ bo/resources/js/ui.common.js
@@ -5,6 +5,7 @@ window.onload = () => {
     handleSideBar();
     handleBookmark();
     handleAllMenu();
+    handleLnb();
     activeTooltip("[data-tooltip]");
 }
 
@@ -34,27 +35,93 @@ function handleSideBar() {
     toggleClass(sidebar, handler, "active");
 }
 
+// lnb메뉴
+function handleLnb() {
+    const lnb = document.querySelector(".lnb_menu");
+    const depth2 = lnb.querySelectorAll(".btn_depth2");
+    const btnMenu = lnb.querySelectorAll(".btn_menu");
+    const test = lnb.querySelector(".has_sub");
+    let prevItem = null;
+
+    // console.log(prevItem);
+
+    btnMenu.forEach((item, index) => {
+        item.addEventListener("click", (e) => {
+            console.log(item.parentElement.parentElement.parentElement);
+            if(!item.classList.contains("btn_depth2")) {
+                // console.log(btnMenu);
+                // btnMenu.closest("li").classList.remove("on");
+                if(prevItem !== null) {
+                    // console.log(btnMenu[prevItem]);
+                    btnMenu[prevItem].closest("li").classList.remove("on");
+                    btnMenu[prevItem].parentElement.parentElement.parentElement.classList.remove("on");
+                }
+                item.closest("li").classList.add("on");
+                
+                if(item.parentElement.parentElement.parentElement === test) {
+                    console.log("dd");
+                    item.parentElement.parentElement.parentElement.classList.add("on");
+                }
+                prevItem = index;
+            }
+        });
+    });
+    
+
+    depth2.forEach((item, index) => {
+        item.addEventListener("click", (e) => {
+            if(!item.closest(".has_sub")) return;
+
+            const defaultHeight = item.offsetHeight;
+            const depth3 = item.nextElementSibling;
+            let depth3Height = depth3.offsetHeight;
+            let openHeight = defaultHeight + depth3Height;
+            // console.log(depth3Height);
+            // let tt = e.target.closest(".has_sub");
+
+            item.closest(".has_sub").classList.toggle("open");
+
+            if(item.closest(".has_sub").classList.contains("open")) {
+                item.closest(".has_sub").style.height = `${openHeight}px`;
+            } else {
+                item.closest(".has_sub").style.height = `${defaultHeight}px`;
+            }
+            // console.log(tt);
+        });
+    });
+}
+
+// lnb 높이
+function setLnbHeight() {
+
+}
+
 // 즐겨찾기
 function handleBookmark() {
     if(document.getElementById("bookmark").length === 0) return;
 
+    const body = document.querySelector("body");
     const bookmark = document.getElementById("bookmark");
     const handler = bookmark.querySelector(".btn_bookmark");
     const listWrap = bookmark.querySelector(".bookmark_list_wrap");
     const list = bookmark.querySelector(".bookmark_list");
     let titleHeight = bookmark.querySelector(".tit").offsetHeight;
     let listWrapHeight = listWrap.offsetHeight;
-    let bookmarkHeight = titleHeight + listWrapHeight;
+    let listHeight = list.offsetHeight;
+    let padding = 16;
+    let bookmarkHeight = titleHeight + listWrapHeight + padding;
 
-    list.childElementCount > 3 ? listWrap.classList.add("shadow") : listWrap.classList.remove("shadow");
+    list.childElementCount > 6 ? bookmark.classList.add("shadow") : bookmark.classList.remove("shadow");
 
     toggleClass(bookmark, handler, "active");
+
+    // console.log(listHeight);
     
     handler.addEventListener("click", () => {
         if(bookmark.classList.contains("active")) {
             bookmark.style.height = `${bookmarkHeight}px`;
         } else {
-            bookmark.style.height = `${titleHeight}px`;
+            bookmark.style.height = `${titleHeight + padding}px`;
         }
     });
 }
@@ -83,6 +150,7 @@ function handleAllMenu() {
 
 // tooltip layer
 function activeTooltip(selector) {
+    const body = document.querySelector("body");
     const wrap = document.querySelector(selector);
     const handler = wrap.querySelector(".tooltip_btn");
     const layer = wrap.querySelector(".tooltip_layer");
@@ -92,6 +160,15 @@ function activeTooltip(selector) {
             wrap.classList.remove("active");
         } else {
             wrap.classList.add("active");
+        }
+    });
+
+    body.addEventListener("click", (e) => {
+        const target = e.target.closest(selector);
+        if(wrap.classList.contains("active")) {
+            if(target !== wrap) {
+                wrap.classList.remove("active");
+            }
         }
     });
 }
