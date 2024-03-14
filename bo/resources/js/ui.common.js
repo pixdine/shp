5adee11445e227faafb7a16fa11f615a3dd89af1
DahyeShin <dhye0513@gmail.com>
2024-03-14 17:54:42 +0900

    js 정리

--- bo/resources/js/ui.common.js
+++ bo/resources/js/ui.common.js
@@ -1,6 +1,6 @@
 const body = document.querySelector("body");
 
-window.executeMacro = function() {
+window.executeLayout = function() {
     let ww = window.innerWidth;
 
     checkDevice(ww);
@@ -8,9 +8,17 @@ window.executeMacro = function() {
     handleBookmark();
     handleAllMenu();
     activeTooltip("[data-tooltip]");
-    leftMenu.init();
-    common.handleRow.init();
-    common.clearText();
+    common.leftMenu.init();
+    common.ui.clearText();
+}
+
+window.executeMacro = function() {
+    let ww = window.innerWidth;
+
+    checkDevice(ww);
+    activeTooltip("[data-tooltip]");
+    common.ui.handleRow.init();
+    common.ui.clearText();
 }
 
 
@@ -20,178 +28,179 @@ window.addEventListener("resize", () => {
 });
 
 const common = {
-    handleRow: {
-        handler: null,
-        handlerTxt: null,
-        table: null,
-        rowHides: null,
-
-        init: function() {
-            if(document.querySelector("#btnShowRow") === null) return;
-
-            handler = document.querySelector("#btnShowRow");
-            handlerTxt = handler.querySelector("span");
-            table = document.querySelector("[data-search]");
-            rowHides = table.querySelectorAll(".row_hide");
-
-            if(rowHides.length === 0) {
-                handler.classList.add("hide");
-            }
-
-            this.initEvent();
-        },
-        initEvent: function() {
-            handler.addEventListener("click", () => {
-                handler.classList.toggle("active");
-
-                rowHides.forEach(item => {
-                    item.classList.toggle("show");
-                });
-
-                this.handleTxt();
-            });
-        },
-        handleTxt: function() {
-            if(handler.classList.contains("active")) {
-                handlerTxt.textContent = "닫기";
-            } else {
-                handlerTxt.textContent = "펼치기";
-            }
-        }
-    },
-    clearText: function() {
-        if(document.querySelectorAll(".inputbox").length === 0) return;
-
-        const inputboxs = document.querySelectorAll(".inputbox");
-        inputboxs.forEach(item => {
-            if(item.querySelector(".btn_ico.clear") === null) return;
-
-            const btnClear = item.querySelector(".btn_ico.clear");
-            const input = item.querySelector("input");
-            
-            input.addEventListener("input", (e) => {
-                if(e.target.value.length > 0) {
-                    item.classList.add("active");
-                } else {
-                    item.classList.remove("active");
-                }
-            });
-            input.addEventListener("focusout", (e) => {
-                item.classList.remove("active");
-            });
-            btnClear.addEventListener("click", () => {
-                input.value = "";
-                input.focus();
-                item.classList.remove("active");
-            });
-        });
-    }
-}
-
-
-// lnb메뉴
-const leftMenu = {
-    lnbWrap : null,
-    lnb: null,
-    btnMenu: null,
-    tooltip: null,
-    newTooltip: null,
-    prevTooltipIndex: null,
-    lnbWrapPosY: null,
-    prevItem: null,
-    depth2: null,
-    
-    init: function() {
-        if(document.querySelector(".lnb_menu_wrap") === null) return;
-
-        lnbWrap = document.querySelector(".lnb_menu_wrap");
-        lnb = document.querySelector(".lnb_menu");
-        depth2 = lnb.querySelectorAll(".btn_depth2");
-        btnMenus = lnb.querySelectorAll(".btn_menu");
-        tooltip = lnbWrap.querySelector(".tooltip_layer");
-        lnbWrapPosY = lnbWrap.getBoundingClientRect().top;
+    // lnb메뉴
+    leftMenu: {
+        lnbWrap : null,
+        lnb: null,
+        btnMenu: null,
+        tooltip: null,
+        newTooltip: null,
+        prevTooltipIndex: null,
+        lnbWrapPosY: null,
+        prevItem: null,
+        depth2: null,
         
-        this.inintEvent();
-        this.handleDepth2();
-        this.clickBody();
-    }, 
+        init: function() {
+            if(document.querySelector(".lnb_menu_wrap") === null) return;
     
-    inintEvent: function() {
-        btnMenus.forEach((item, index) => {
-            // 활성화
-            item.addEventListener("click", () => {
-                if(!item.classList.contains("btn_depth2")) {
-                    if(this.prevItem !== null) {
-                        btnMenus[this.prevItem].closest("li").classList.remove("on");
-                        btnMenus[this.prevItem].parentElement.parentElement.parentElement.classList.remove("on");
-                        this.prevItem = null;
+            lnbWrap = document.querySelector(".lnb_menu_wrap");
+            lnb = document.querySelector(".lnb_menu");
+            depth2 = lnb.querySelectorAll(".btn_depth2");
+            btnMenus = lnb.querySelectorAll(".btn_menu");
+            tooltip = lnbWrap.querySelector(".tooltip_layer");
+            lnbWrapPosY = lnbWrap.getBoundingClientRect().top;
+            
+            this.inintEvent();
+            this.handleDepth2();
+            this.clickBody();
+        }, 
+        
+        inintEvent: function() {
+            btnMenus.forEach((item, index) => {
+                // 활성화
+                item.addEventListener("click", () => {
+                    if(!item.classList.contains("btn_depth2")) {
+                        if(this.prevItem !== null) {
+                            btnMenus[this.prevItem].closest("li").classList.remove("on");
+                            btnMenus[this.prevItem].parentElement.parentElement.parentElement.classList.remove("on");
+                            this.prevItem = null;
+                        }
+                        item.closest("li").classList.add("on");
+                        if(item.closest("li.has_sub")) {
+                            item.closest("li.has_sub").classList.add("on");
+                        }
+                        this.prevItem = index;
                     }
-                    item.closest("li").classList.add("on");
-                    if(item.closest("li.has_sub")) {
-                        item.closest("li.has_sub").classList.add("on");
+                });
+    
+                // 우클릭
+                item.addEventListener("contextmenu", (e) => {
+                    e.preventDefault();
+    
+                    this.cloneTooltip();
+                    this.hideTooltip();
+                    this.openTooltip(item, index);
+                });
+            });
+        },
+    
+        // 2depth 접기/펼치기
+        handleDepth2: function() {
+            depth2.forEach((item, index) => {
+                item.addEventListener("click", (e) => {
+                    if(!item.closest(".has_sub")) return;
+        
+                    const defaultHeight = item.offsetHeight;
+                    const depth3 = item.nextElementSibling;
+                    let depth3Height = depth3.offsetHeight;
+                    let openHeight = defaultHeight + depth3Height;
+        
+                    item.closest(".has_sub").classList.toggle("open");
+        
+                    if(item.closest(".has_sub").classList.contains("open")) {
+                        item.closest(".has_sub").style.height = `${openHeight}px`;
+                    } else {
+                        item.closest(".has_sub").style.height = `${defaultHeight}px`;
                     }
-                    this.prevItem = index;
+                });
+            });
+        },
+        openTooltip: function(item, index) {
+            if(item.hasAttribute("data-tooltip")) {
+                if(!item.parentElement.classList.contains("active")) {
+                    item.parentElement.classList.add("tooltip_layer_wrap", "active");
+                    item.parentElement.append(this.newTooltip);
+                    this.prevTooltipIndex = index;
                 }
-            });
-
-            // 우클릭
-            item.addEventListener("contextmenu", (e) => {
-                e.preventDefault();
-
-                this.cloneTooltip();
-                this.hideTooltip();
-                this.openTooltip(item, index);
-            });
-        });
-    },
-
-    // 2depth 접기/펼치기
-    handleDepth2: function() {
-        depth2.forEach((item, index) => {
-            item.addEventListener("click", (e) => {
-                if(!item.closest(".has_sub")) return;
-    
-                const defaultHeight = item.offsetHeight;
-                const depth3 = item.nextElementSibling;
-                let depth3Height = depth3.offsetHeight;
-                let openHeight = defaultHeight + depth3Height;
-    
-                item.closest(".has_sub").classList.toggle("open");
-    
-                if(item.closest(".has_sub").classList.contains("open")) {
-                    item.closest(".has_sub").style.height = `${openHeight}px`;
-                } else {
-                    item.closest(".has_sub").style.height = `${defaultHeight}px`;
-                }
-            });
-        });
-    },
-    openTooltip: function(item, index) {
-        if(item.hasAttribute("data-tooltip")) {
-            if(!item.parentElement.classList.contains("active")) {
-                item.parentElement.classList.add("tooltip_layer_wrap", "active");
-                item.parentElement.append(this.newTooltip);
-                this.prevTooltipIndex = index;
             }
+        },
+        cloneTooltip: function() {
+            this.newTooltip = tooltip.cloneNode(true);
+        },
+        hideTooltip: function() {
+            if(this.prevTooltipIndex !== null) {
+                // console.log(this.prevTooltipIndex);
+                let prevMenuParentEl = btnMenus[this.prevTooltipIndex].parentElement;
+                prevMenuParentEl.classList.remove("tooltip_layer_wrap", "active");
+                prevMenuParentEl.querySelector(".tooltip_layer").remove();
+                this.prevTooltipIndex = null;
+            }
+        },
+        clickBody: function() {
+            body.addEventListener("click", (e) => {
+                this.hideTooltip();
+            });
         }
     },
-    cloneTooltip: function() {
-        this.newTooltip = tooltip.cloneNode(true);
-    },
-    hideTooltip: function() {
-        if(this.prevTooltipIndex !== null) {
-            // console.log(this.prevTooltipIndex);
-            let prevMenuParentEl = btnMenus[this.prevTooltipIndex].parentElement;
-            prevMenuParentEl.classList.remove("tooltip_layer_wrap", "active");
-            prevMenuParentEl.querySelector(".tooltip_layer").remove();
-            this.prevTooltipIndex = null;
+    ui: {
+        handleRow: {
+            handler: null,
+            handlerTxt: null,
+            table: null,
+            rowHides: null,
+    
+            init: function() {
+                if(document.querySelector("#btnShowRow") === null) return;
+    
+                handler = document.querySelector("#btnShowRow");
+                handlerTxt = handler.querySelector("span");
+                table = document.querySelector("[data-search]");
+                rowHides = table.querySelectorAll(".row_hide");
+    
+                if(rowHides.length === 0) {
+                    handler.classList.add("hide");
+                }
+    
+                this.initEvent();
+            },
+            initEvent: function() {
+                handler.addEventListener("click", () => {
+                    handler.classList.toggle("active");
+    
+                    rowHides.forEach(item => {
+                        item.classList.toggle("show");
+                    });
+    
+                    this.handleTxt();
+                });
+            },
+            handleTxt: function() {
+                if(handler.classList.contains("active")) {
+                    handlerTxt.textContent = "닫기";
+                } else {
+                    handlerTxt.textContent = "펼치기";
+                }
+            }
+        },
+        clearText: function() {
+            if(document.querySelectorAll(".inputbox").length === 0) return;
+    
+            const inputboxs = document.querySelectorAll(".inputbox");
+            inputboxs.forEach(item => {
+                if(item.querySelector(".btn_ico.clear") === null) return;
+    
+                const btnClear = item.querySelector(".btn_ico.clear");
+                const input = item.querySelector("input");
+                
+                input.addEventListener("input", (e) => {
+                    if(e.target.value.length > 0) {
+                        item.classList.add("active");
+                    } else {
+                        item.classList.remove("active");
+                    }
+                });
+                input.addEventListener("focusout", (e) => {
+                    item.classList.remove("active");
+                });
+                btnClear.addEventListener("click", () => {
+                    input.value = "";
+                    input.focus();
+                    item.classList.remove("active");
+                });
+            });
         }
-    },
-    clickBody: function() {
-        body.addEventListener("click", (e) => {
-            this.hideTooltip();
-        });
     }
+    
 }
 
 // 디바이스 체크
